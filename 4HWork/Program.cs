79c2b5cf440aba317999b9e1f21c9d65a27d3d72
Nozim Yokubov <anorcomander@gmail.com>
2024-04-07 10:36:07 +0500

    added 4HWork

---
+++ 4HWork/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 4*(не обязательная): Задайте строку, состоящую из слов, разделенных пробелами. 
+// Сформировать строку, в которой слова расположены в обратном порядке. 
+// В полученной строке слова должны быть также разделены пробелами.
+
+// “Hello my world” => “world my Hello”
+
+string hello = "Hello my world";
+string res = string.Empty;
+string[] helloSplit = hello.Split();//["Hello", "my", "world"]
+
+for (int i = helloSplit.Length - 1; i >= 0; i--)
+{
+res += helloSplit[i] + " ";
+}
+
+Console.WriteLine(res.TrimEnd());
